658de1dca5b41c094ee26253157e87a72908b242
Thomas Kastner <tom@sprungknoedl.at>
2016-02-26 16:34:59 +0100

    return database in specified format

--- handlers.go
+++ handlers.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"encoding/csv"
 	"fmt"
 	"net/http"
+	"strconv"
 	"time"
 )
 
@@ -15,14 +17,28 @@ func GetDatabase(w http.ResponseWriter, r *http.Request) {
 	ctx := NewContext(r)
 	ch := Find(ctx, nil)
 
+	w.Header().Add("content-type", "text/csv")
+	writer := csv.NewWriter(w)
+
 	for entry := range ch {
 		if entry.Err != nil {
 			HandleError(w, r, entry.Err)
 			return
 		}
 
-		fmt.Fprintf(w, "%+v\n", entry)
+		// csv format
+		// source,domain,ip4,last,category,description
+		writer.Write([]string{
+			entry.Source,
+			entry.Domain,
+			entry.IP4,
+			strconv.FormatInt(entry.Last.Unix(), 10),
+			entry.Category,
+			entry.Description,
+		})
 	}
+
+	writer.Flush()
 }
 
 func GetCron(w http.ResponseWriter, r *http.Request) {
